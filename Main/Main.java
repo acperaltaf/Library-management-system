cd94672e9573ef1f3ae9a1afdba54ed4a5156a62
Andres Peralta <andresperalta0042@gmail.com>
2025-01-22 11:51:44 -0500

    clase Menu esta aparentemente completa

--- Main/Main.java
+++ Main/Main.java
@@ -1,60 +1,41 @@
 package Main;
 
-import Book.Book;
-import UI.Menu;
 import Library.Library;
+import UI.Menu;
+import Book.Book;
 import User.User;
 
 public class Main {
     public static void main(String[] args) {
-        // Menu.showMenu();
+        /*
         Book book1 = new Book("Cien años de soledad", "Gabriel García Marquez", "123456-09", true);
+        Book book3 = new Book("El señor de los anillos","J. R. R.Tolkien", "2468-70", true);
         Book book2 = new Book("Juego de tronos", "George R. R. Martin", "654321-90", true);
+        Book book4 = new Book("Don Quijote De La Mancha","Miguel de Cervantes Saavedra", "13579-71", true);
+        Book book5 = new Book("Cronicas de una muerte anuncida","Gabriel García Márquez", "99123-34", true);
+        
+        // Instantiate the Library
         Library library = new Library();
         
+        // Instatiante the User
+        User user = new User("Andres", "a01");
+
+        // Add the books to the library
         library.addBook(book1);
         library.addBook(book2);
-
-        /* 
-        System.out.println("-- -- List of books -- --");
-        for (Book book : library.getListOfBooks()) {
-            System.out.println(book);
-        }
-        */
-
-        // ----------------------------
-
-        User user = new User("Andres", "a01");
-        library.registerUser(user);
-
-        Book book3 = new Book("El señor de los anillos","J. R. R.Tolkien", "2468-70", true);
-        
-        user.addBorrowedBook(book2);
-        user.addBorrowedBook(book3);
-        user.removeBorrowedBook(book2);
-        
-        /* Comment */
-        // System.out.println(user.getListOfBorrowedBooks());
-        for (Book book : user.getListOfBorrowedBooks()) {
-            System.out.println(book);
-        }
-        
-        Book book4 = new Book("Don Quijote De La Mancha","Miguel de Cervantes Saavedra", "13579-71", true);
+        library.addBook(book3);
         library.addBook(book4);
-        Book book5 = new Book("Cronicas de una muerte anuncida","Gabriel García Márquez", "99123-34", true);
         library.addBook(book5);
-        library.lendBook(book5.getIsbn(), user.getID());
-
-        library.lendBook(book4.getIsbn(), user.getID());
-        System.out.println("Book 4 availability -> " + book4.getIsAvailable());
         
-        library.returnBook(book4.getIsbn(),user.getID());
-        System.out.println("Book 4 availability -> " + book4.getIsAvailable());
-
-        System.out.println("-0-0-0-0-0-0-0-0-0-0-00-0-0-");
-        library.listAvailableBooks();
+        // Register user in the library
+        library.registerUser(user); 
+         */
         
-        System.out.println("-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-");
-        library.listBorrowedBooks();
+         Library library = new Library();
+         Menu menu = new Menu(library);
+
+        //  menu.showMenu();
+         menu.handleOption();
+        
     } // End main
 } // End Main
--- UI/Menu.java
+++ UI/Menu.java
@@ -1,15 +1,102 @@
 package UI;
 
+import java.util.Scanner;
+import Library.Library;
+import Book.Book;
+import User.User;
+
 public class Menu {
-    public static void showMenu() {
+
+    private Library library;
+    private Scanner scanner;
+
+    public Menu(Library library) {
+        this.library = library;
+        this.scanner = new Scanner(System.in);
+    }
+
+    public void showMenu() {
         System.out.println("""
-                1. Añadir libro a la biblioteca.
-                2. Registrar usuario.
-                3. Prestar libro.
-                4. Devolver libro.
-                5. Mostrar libros disponibles.
-                6. Mostrar libros prestados.
-                7. Salir.
+                1. Add book to library.
+                2. Register user.
+                3. Lend book.
+                4. Retunr book.
+                5. Show available books.
+                6. Show borrowed books.
+                7. Exit.
                 """);
+    }
+
+    public void handleOption() {
+        int option = 0;
+        do {
+            showMenu();
+            System.out.print("Select an option: ");
+            option = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (option) {
+                case 1: // Add book to library.
+                    addBook();
+                    break;
+                case 2: // Register user.
+                    registerUser();
+                    break;
+                case 3: // Lend book
+                    lendBook();
+                    break;
+                case 4: // Retunr book
+                    returnBook();
+                    break;
+                case 5: // Show available books
+                    library.listAvailableBooks();
+                    break;
+                case 6: // Show borrowed books
+                    library.listBorrowedBooks();
+                    break;
             }
-        }
+
+        } while (option != 7);
+
+        // Closed the scanner
+        scanner.close();
+    }
+
+    private void addBook() {
+        System.out.print("Enter book title: ");
+        String title = scanner.nextLine();
+        System.out.print("Enter book author: ");
+        String author = scanner.nextLine();
+        System.out.print("Enter book ISBN: ");
+        String isbn = scanner.nextLine();
+        Book book = new Book(title, author, isbn, true);
+        library.addBook(book);
+        System.out.println("Book added successfully");
+    }
+
+    private void registerUser() {
+        System.out.print("Enter user name: ");
+        String name = scanner.nextLine();
+        System.out.print("Enter user ID: ");
+        String userId = scanner.nextLine();
+        User user = new User(name, userId);
+        library.registerUser(user);
+        System.out.println("User added successfully");
+    }
+
+    private void lendBook() {
+        System.out.print("Enter book ISBN: ");
+        String isbn = scanner.nextLine();
+        System.out.print("Enter user ID: ");
+        String userId = scanner.nextLine();
+        library.lendBook(isbn, userId);
+    }
+
+    private void returnBook() {
+        System.out.print("Enter book ISBN: ");
+        String isbn = scanner.nextLine();
+        System.out.print("Enter user ID: ");
+        String userId = scanner.nextLine();
+        library.returnBook(isbn, userId);
+    }
+} // End Menu
